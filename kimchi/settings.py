50a23b367df8ffa1672dd90affbd2c554886e9b7
stewarthaha <gonext1@naver.com>
2018-09-03 00:08:44 +0630

    change Template Directory option at settings.py

--- kimchi/settings.py
+++ kimchi/settings.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'kimchi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#       'DIRS': [os.path.join(BASE_DIR,'templates')],
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+#        'DIRS': [],   " Origin" 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
